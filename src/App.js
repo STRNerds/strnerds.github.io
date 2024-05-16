3f1698e7f1c942104ca4c217076b79c87a7d9cef
Penguin60 <radean.rashed@gmail.com>
2024-05-16 15:31:21 -0400

    Add homepage layout

--- src/App.js
+++ src/App.js
@@ -1,49 +1,104 @@
 import { Box } from "@mui/joy";
 import "./App.css";
+import Typed from "typed.js";
+import React from "react";
 
 function App() {
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["NERDS", "DEVELOPERS", "STUDENTS"],
+      typeSpeed: 50,
+      backSpeed: 40,
+      backDelay: 2000,
+      loop: true,
+      showCursor: false,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <>
       <Box
-        className="navbar"
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '95%',
-          padding: 0.1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <Box
+          className="navbar"
           sx={{
-            display: 'flex',
-            justifyContent: 'flex-start', // Align items to the start
-            alignItems: 'center',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "95%",
+            padding: 0.1,
           }}
         >
-          <img src="nerd.png" className="nerdNavbar" />
-          <h3 className="homeButton" sx={{ marginBottom: '0' }}>STRNerds</h3>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            gap: 3,
-            mr: 3,
-          }}
-        >
-          <h3 className="projectsButton" sx={{ marginBottom: '0' }}>Projects</h3>
-          <h3 className="aboutUsButton" sx={{ marginBottom: '0' }}>About Us</h3>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start", // Align items to the start
+              alignItems: "center",
+            }}
+          >
+            {/* <img src="nerd.png" className="nerdNavbar" /> */}
+            <h3 className="homeButton" sx={{ marginBottom: "0" }}>
+              STRNerds
+            </h3>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              gap: 3,
+              mr: 4,
+            }}
+          >
+            <h3 className="projectsButton" sx={{ marginBottom: "0" }}>
+              Projects
+            </h3>
+            <h3 className="aboutUsButton" sx={{ marginBottom: "0" }}>
+              About Us
+            </h3>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          justifyItems: "center",
+          mt: 17,
+        }}
+      >
+        <h1 className="weAre">We are</h1>
+        <h1 ref={el} className="heroText" style={{ minHeight: "1.2em" }} />{" "}
+        {/* Add this line */}
+        <h1 className="description">
+          Who work together to create useful <br /> and interesting software.
+        </h1>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          ml: 110,
+          mt: -17.8,
+        }}
+      >
+        <img src="nerdsvg.svg" className="nerdSvg" />
+      </Box>
+    </>
   );
 }
 
