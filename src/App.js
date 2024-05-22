2b2935d3463cf75329863a4ae3178297ead7b7a6
aqariio <aquario.mw@gmail.com>
2024-05-21 22:51:38 -0400

    Add about us page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProjectPage from "./components/ProjectPage";
 import MainMenu from "./components/MainMenu";
 import Navbar from "./components/Navbar";
+import AboutUsPage from "./components/AboutUsPage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainMenu />} />
           <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/about" element={<AboutUsPage />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/AboutUsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AboutUsPage.css";
+
+const AboutUsPage = () => {
+  return (
+    <div className="omni">
+      <h1>About Us</h1>
+      <p>STRNerds is a small group of students who are learning computer science</p>
+    </div>
+  );
+}
+
+export default AboutUsPage;
