f06b6d51cb57c4202dfdcb01befedc3f4b4465fd
Ethan Hu <hu.ethan.ca@gmail.com>
2024-05-23 11:29:08 -0400

    Redesigned about us page

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -3,10 +3,35 @@ import "./AboutUsPage.css";
 
 const AboutUsPage = () => {
   return (
-    <div className="omni">
-      <h1>About Us</h1>
-      <p>STRNerds is a small group of students who are learning computer science</p>
-    </div>
+    <>
+      <div>
+        <h1 className="AboutUsHeader">About Us</h1>
+        <p className="AboutUsDescription">STRNerds is a small group of students who are learning computer science</p>
+      </div>
+      <div className="people">
+        <div className="profiles">
+            <img src="bomber.png" id="Rad0" className="pfp"></img>
+            <p className="profileDescription">THE Progammer</p>
+        </div>
+        <div className="profiles">
+            <img src="bomber.png" id="Rad1" className="pfp"></img>
+            <p className="profileDescription">THE Progammer</p>
+        </div>
+        <div className="profiles">
+            <img src="bomber.png" id="Rad2" className="pfp"></img>
+            <p className="profileDescription">THE Progammer</p>
+        </div>
+        <div className="profiles">
+            <img src="bomber.png" id="Rad3" className="pfp"></img>
+            <p className="profileDescription">THE Progammer</p>
+        </div>
+        <div className="profiles">
+            <img src="bomber.png" id="Rad4" className="pfp"></img>
+            <p className="profileDescription">THE Progammer</p>
+        </div> 
+      </div>
+    </>
+    
   );
 }
 
