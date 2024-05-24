c3b090db48f1b86dbc50548a1e59aff9874406d5
Ethan Yan Xu <ethanxucoder@gmail.com>
2024-05-24 09:36:53 -0400

    Fix format and secret page key combination issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import ProjectPage from "./components/ProjectPage";
 import MainMenu from "./components/MainMenu";
 import Navbar from "./components/Navbar";
 import AboutUsPage from "./components/AboutUsPage";
 import SecretPage from "./components/SecretPage";
-
-const App = () => {
-  return (
-    <>
-      <Router>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<MainMenu />} />
-          <Route path="/projects" element={<ProjectPage />} />
-          <Route path="/about" element={<AboutUsPage />} />
-          <Route path="/kimixu" element={<SecretPage />}/>
-        </Routes>
-      </Router>
-    </>
-  );
+const App=()=>{
+ return (<>
+   <Router>
+    <Navbar/>
+    <Routes>
+     <Route path="/" element={<MainMenu/>}/>
+     <Route path="/projects" element={<ProjectPage/>}/>
+     <Route path="/about" element={<AboutUsPage/>}/>
+     <Route path="/ihuwiufhauifawhfuiwahfihfiu" element={<SecretPage/>}/>
+    </Routes>
+   </Router>
+  </>);
 }
-
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,41 @@
-import { Box } from "@mui/joy";
+import {Box} from "@mui/joy";
 import React from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
-
-const Navbar = () => {
-  return (
+import {Link} from "react-router-dom";
+const Navbar=()=>{
+ return (<Box
+   sx={{
+    display:"flex",alignItems:"center",justifyContent:"center",
+   }}
+  >
+   <Box
+    className="navbar"
+    sx={{
+     display:"flex",justifyContent:"space-between",alignItems:"center",width:"95%",padding:"0.1vh",
+    }}
+   >
     <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+     sx={{
+      display:"flex",justifyContent:"flex-start",alignItems:"center",
+     }}
     >
-      <Box
-        className="navbar"
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "95%",
-          padding: "0.1vh",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-          }}
-        >
-          <Link to="/" className="homeButton" sx={{ marginBottom: "0" }}>
-            STRNerds
-          </Link>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            gap: "7vh",
-            mr: "2vh",
-          }}
-        >
-          <Link to="/projects" className="projectsButton" sx={{ marginBottom: "0" }}>
-            Projects
-          </Link>
-          <Link to="/about" className="aboutUsButton" sx={{ marginBottom: "0" }}>
-            About Us
-          </Link>
-        </Box>
-      </Box>
+     <Link to="/" className="homeButton" sx={{marginBottom:"0"}}>
+      STRNerds
+     </Link>
     </Box>
-  );
+    <Box
+     sx={{
+      display:"flex",justifyContent:"flex-end",alignItems:"center",gap:"7vh",mr:"2vh",
+     }}
+    >
+     <Link to="/projects" className="projectsButton" sx={{marginBottom:"0"}}>
+      Projects
+     </Link>
+     <Link to="/about" className="aboutUsButton" sx={{marginBottom:"0"}}>
+      About Us
+     </Link>
+    </Box>
+   </Box>
+  </Box>);
 };
-
 export default Navbar;
